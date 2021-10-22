c1cf33d3d5ab1e08e9a5afef9e3d8e42525543c8
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2021-10-22 11:34:03 +0200

    more robust searching

--- sökfunktion/search.js
+++ sökfunktion/search.js
@@ -2,16 +2,20 @@ let noPages = null;
 const APIKey = 'ABGhAz7mHz2uYymBiuGQUhBYc4taw5KF';
 const main = document.querySelector('main');
 
+
 // Get search from API and create HTML-list with results
-window.addEventListener('DOMContentLoaded', async function(event) {
-	let url = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?offset=0&order=by-publication-date&api-key=${APIKey}`;
-	let searchTerm = 'harry potter';
-	url = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?order=by-publication-date&query=${searchTerm}&api-key=${APIKey}`;
+window.addEventListener('DOMContentLoaded', () => {
+	let input = '  harry potter     and the   ';
+	input = cleanUpInput(input);
+	search(input);
+});
+
+const search = async (input) => {
+	let url = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?order=by-publication-date&query=${input}&api-key=${APIKey}`;
+	
 	let response = await fetch(url);
 	let data = await response.json();
 	let resultsArray = data.results;
-	
-	console.log(resultsArray);
 	
 	let resultsList = document.createElement('ul');
 	
@@ -23,10 +27,14 @@ window.addEventListener('DOMContentLoaded', async function(event) {
 		})
 	}
 	main.append(resultsList);
-	console.log(data);
-});
+}
 
-// 3860 is current last offset-page
+const cleanUpInput = (input) => {
+	input = input.trim();
+	input = input.replace(/\s\s+/g, ' ');
+	input = input.substring(0, 150);
+	return input;
+}
 
 // https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=ABGhAz7mHz2uYymBiuGQUhBYc4taw5KF
 
